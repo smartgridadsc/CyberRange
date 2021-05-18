fa41becb1552ceec6f31713627633e20e54b2ba0
Huangwz <74039374+huangwz-dev@users.noreply.github.com>
2021-05-18 14:50:10 +0800

    Delete Demo V1 directory

--- Demo V1/IED/IED Configurator v.2.2/testing3.py
+++
@@ -1,9 +0,0 @@
-# import subprocess
-#
-# subprocess.call('./home/plc/Desktop/create/Makefile')
-
-import os
-
-commands = "cd /home/plc/Desktop/create/\nmake\n"
-
-os.system(commands)
--- Demo V1/SCD-Mininet/scd-Mininet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo python3 -E scd_parser.py 
-
-sudo python3 -E run.py
- 
-
