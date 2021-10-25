7d18461ca42419ab0e0f538f099eeb881b2ada7d
wenshei <42404058+wenshei@users.noreply.github.com>
2021-10-25 12:13:03 +0800

    Update MMS.cpp
    Updated constructor for storing control parameters into class member.
    Modified control handler.

--- VirtualIED/CommModules/MMS.cpp
+++ VirtualIED/CommModules/MMS.cpp
@@ -18,6 +18,7 @@ using namespace std;
 MMSModule::MMSModule() {
     LOG(DEBUG, "Creating MMSModule >>>>>>>>>>>>>>>>>\n");
 
+    printf("here\n");
     iedServer = IedServer_create(&iedModel);
     interval = MMS_INTERVAL;
 
@@ -26,14 +27,20 @@ MMSModule::MMSModule() {
     //Control Handlers
     //HARD-CODING
     //TO DO: dynamic control handler setting
-    vector<void*> ctrl_param;
-    ctrl_param.push_back(&iedServer);
-    ctrl_param.push_back(IEDMODEL_LD1_GGIO1_SPC1);
-    //ctrl_param.push_back(IEDMODEL_LogicalDevice_GGIO3_SPC1);
-    ctrl_param.push_back(db_conn);
-    IedServer_setControlHandler(iedServer, IEDMODEL_LD1_GGIO1_SPC1, (ControlHandler)MMSModule::control_handler, &ctrl_param);
-    //IedServer_setControlHandler(iedServer, IEDMODEL_LogicalDevice_GGIO3_SPC1, (ControlHandler)MMSModule::control_handler, &ctrl_param);
+    vector<void*> *ctrl_param1 = new vector<void*>();
+    ctrl_param1->push_back(&iedServer);
+    ctrl_param1->push_back(IEDMODEL_CTRL_XCBR1_Pos);
+    ctrl_param1->push_back(db_conn);
+    controlParams.push_back(ctrl_param1);
+    IedServer_setControlHandler(iedServer, IEDMODEL_CTRL_XCBR1_Pos, (ControlHandler)MMSModule::control_handler, controlParams[0]);
 
+    vector<void*> *ctrl_param2 = new vector<void*>();
+    ctrl_param2->push_back(&iedServer);
+    ctrl_param2->push_back(IEDMODEL_CTRL_XCBR2_Pos);
+    ctrl_param2->push_back(db_conn);
+    controlParams.push_back(ctrl_param2);
+    IedServer_setControlHandler(iedServer, IEDMODEL_CTRL_XCBR2_Pos, (ControlHandler)MMSModule::control_handler, controlParams[1]);
+    
     LOG(DEBUG, ">>>>>>>>>>>>>>>>>> MMSModule created\n");
 }
 
@@ -90,6 +97,7 @@ ControlHandlerResult MMSModule::control_handler(ControlAction action, void *para
     }
 
     vector<void*> *ctrl_param = (vector<void*> *)parameter;
+    //hardcoded 
     if (ctrl_param->size() != 3) {
         LOG(ERROR, "Control_handler() failed! Parameters must be 3 (current: %u)\n", ctrl_param->size());
         return CONTROL_RESULT_FAILED;
@@ -101,12 +109,21 @@ ControlHandlerResult MMSModule::control_handler(ControlAction action, void *para
 
     uint64_t timeStamp = Hal_getTimeInMs();
     //if (data_attr == IEDMODEL_LogicalDevice_GGIO3_SPC1) {
-    if (data_attr == IEDMODEL_LD1_GGIO1_SPC1) {
-        lock_guard<mutex> lock(static_model_lock);
-        IedServer_updateUTCTimeAttributeValue(
-                *iedServer, IEDMODEL_LD1_GGIO1_SPC1_t, timeStamp);
-        IedServer_updateAttributeValue(
-                *iedServer, IEDMODEL_LD1_GGIO1_SPC1_stVal, value);
+    if (data_attr == IEDMODEL_CTRL_XCBR1_Pos) {
+        { //limit scope of mutex
+            lock_guard<mutex> lock(static_model_lock);
+            IedServer_updateUTCTimeAttributeValue(
+                    *iedServer, IEDMODEL_CTRL_XCBR1_Pos_t, timeStamp);
+
+            //for bit-string MmsValue (not working, must find out bitstring encoding)
+            // MmsValue *newStVal = MmsValue_newBitString(4); //not sure what is the proper length of bitstring (may not be 8)
+            // if (MmsValue_getBoolean(value) == true) 
+            //     MmsValue_setBitStringFromInteger(newStVal, 1); //on = 1, based in enum value defined in MIED1.icd
+            // else 
+            //     MmsValue_setBitStringFromInteger(newStVal, 5); //off = 5, based in enum value defined in MIED1.icd
+            // IedServer_updateAttributeValue( 
+            //         *iedServer, IEDMODEL_CTRL_XCBR1_Pos_stVal, newStVal);
+        }
 
         //write to database
         //TO DO: get
@@ -115,10 +132,10 @@ ControlHandlerResult MMSModule::control_handler(ControlAction action, void *para
             querystring = "UPDATE line_cb SET value=1 WHERE name=\"line_cb_0_1\"";
         else
             querystring = "UPDATE line_cb SET value=0 WHERE name=\"line_cb_0_1\"";
-        LOG(DEBUG, "Writing to database: [%s]\n", querystring)
+        LOG(DEBUG, "Writing to database: [%s]\n", querystring.c_str())
         try {
-            mysqlpp::Query query = db_conn->conn.query(querystring);
-            query.exec();
+            //mysqlpp::Query query = db_conn->conn.query(querystring);
+            //query.exec();
         }
         catch (mysqlpp::Exception e) {
             LOG(DEBUG, "Exception when writing to database\n");
